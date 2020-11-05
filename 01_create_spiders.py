13bca46212968127f0150f62b53313bbd9a83417
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-11-05 19:24:17 -0300

    Cities checked and spiders created

--- 01_create_spiders.py
+++ 01_create_spiders.py
@@ -24,15 +24,15 @@ def load_territories():
 
 
 def load_cities():
-    with open("cities_ok.csv") as csvfile:
+    with open("checked.csv") as csvfile:
         rows = csv.reader(csvfile, delimiter=",")
-        return [[state, city] for [state, city, _] in rows]
+        return [[state, city, url] for [state, city, url] in rows]
 
 
 def save_spiders(directory, matched_cities):
     template_file = open("spider_template", mode="r")
     template = template_file.read()
-    for (territory_id, state, city, city_full) in matched_cities:
+    for (territory_id, state, city, city_full, _) in matched_cities:
         filename = f"{state}_{city}.py"
         class_name = get_class_name(state, city_full)
         print("Spawning", class_name, "...")
@@ -54,19 +54,24 @@ def main():
     directory = os.path.join("spiders", datetime.now().strftime("%Y_%m_%d_%s"))
     os.makedirs(directory)
 
-    for (state, city) in cities:
+    for (state, city, url) in cities:
         for (territory_id, t_city, t_city_name, t_state) in territories:
             if t_city == city and t_state == state:
-                matched.append((territory_id, state, city, t_city_name))
+                matched.append((territory_id, state, city, t_city_name, url))
                 break
         else:
             not_matched.append([state, city])
     save_spiders(directory, matched)
-    # print(not_matched)
-    # with open(os.path.join(directory, "not_matched.csv"), "w") as not_matched_file:
-    x = [f"{state},{city}\n" for [state, city] in not_matched]
-    with open(os.path.join(directory, "0_not_matched.csv"), "w") as not_matched_file:
-        not_matched_file.writelines(x)
+
+    with open(os.path.join(directory, "00_not_matched.csv"), "w") as not_matched_file:
+        not_matched_file.writelines(
+            [f"{state},{city}\n" for [state, city] in not_matched]
+        )
+
+    with open(os.path.join(directory, "01_matched.csv"), "w") as matched_file:
+        matched_file.writelines(
+            [f"{state},{city},{url}\n" for [_, state, city, _, url] in matched]
+        )
 
 
 if __name__ == "__main__":
