544bc05f6f800cf611d10f2db633d07612b23a2e
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-11-05 17:40:01 -0300

    Save cities not matched

--- 01_create_spiders.py
+++ 01_create_spiders.py
@@ -29,11 +29,9 @@ def load_cities():
         return [[state, city] for [state, city, _] in rows]
 
 
-def save_spiders(matched_cities):
+def save_spiders(directory, matched_cities):
     template_file = open("spider_template.txt", mode="r")
     template = template_file.read()
-    dir = datetime.now().strftime("%Y_%m_%d_%s")
-    os.mkdir(dir)
     for (territory_id, state, city, city_full) in matched_cities:
         filename = f"{state}_{city}.py"
         class_name = get_class_name(state, city_full)
@@ -44,7 +42,7 @@ def save_spiders(matched_cities):
             .replace("{state}", state)
             .replace("{city}", city)
         )
-        with open(os.path.join(dir, filename), "w") as spider_file:
+        with open(os.path.join(directory, filename), "w") as spider_file:
             spider_file.write(spider_code)
 
 
@@ -53,15 +51,22 @@ def main():
     matched = []
     not_matched = []
     cities = load_cities()
-    print(cities)
+    directory = os.path.join("spiders", datetime.now().strftime("%Y_%m_%d_%s"))
+    os.mkdir(directory)
+
     for (state, city) in cities:
         for (territory_id, t_city, t_city_name, t_state) in territories:
             if t_city == city and t_state == state:
                 matched.append((territory_id, state, city, t_city_name))
                 break
         else:
-            not_matched.append((state, city))
-    save_spiders(matched)
+            not_matched.append([state, city])
+    save_spiders(directory, matched)
+    # print(not_matched)
+    # with open(os.path.join(directory, "not_matched.csv"), "w") as not_matched_file:
+    x = [f"{state},{city}\n" for [state, city] in not_matched]
+    with open(os.path.join(directory, "0_not_matched.csv"), "w") as not_matched_file:
+        not_matched_file.writelines(x)
 
 
 if __name__ == "__main__":
